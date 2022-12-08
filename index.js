c180787562b7a14b5454ad5bd601e46aa9a0bcf8
Vaibhav Suryawanshi <vaibhavsuryawanshi015@gmail.com>
2022-12-08 17:11:06 +0530

    done

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ const app = express();
 mongoose.set('strictQuery', true);
 app.set('view engine', 'ejs');
 
-
+app.use(express.static(path.join(__dirname, "./client_side/build")))
+app.get("*", function(_, res){
+    res.sendFile(path.join(__dirname, "./client_side/build/index.html"),
+    function(err){
+        res.send(err)
+    })
+})
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors({
