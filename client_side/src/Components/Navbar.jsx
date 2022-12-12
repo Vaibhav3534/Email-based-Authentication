874eef6fb97a5b4de820579d2a47eac18047b2c8
Vaibhav Suryawanshi <vaibhavsuryawanshi015@gmail.com>
2022-12-13 01:41:38 +0530

    doone

--- client_side/src/Components/Navbar.jsx
+++ client_side/src/Components/Navbar.jsx
@@ -22,13 +22,13 @@ export default function ButtonAppBar() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            {/* <MenuIcon /> */}
+            <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Email Authentication App
           </Typography>
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">Login</Button>
+          {/* <Button color="inherit">Login</Button>
+          <Button color="inherit">Login</Button> */}
         </Toolbar>
       </AppBar>
     </Box>
